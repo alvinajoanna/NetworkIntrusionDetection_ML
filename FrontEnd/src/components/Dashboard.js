8529070a56214fa5ec3f9292344599f2d9283ed6
Nixon-Joji <123238738+Nixon-Joji@users.noreply.github.com>
2023-05-03 14:11:01 +0530

    Add files via upload

--- FrontEnd/src/components/Dashboard.js
+++ FrontEnd/src/components/Dashboard.js
@@ -1,21 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from './Card';
 import Navbar from './Navbar';
 import './Dashboard.css';
 import Visualization from "./Visualization";
 
 const Dashboard = () => {
+  const [currentpage, setcurrentpage] = useState("main");
+
+
   return (
     <div className="dashboard">
-      <Navbar></Navbar>
-      <div className="card-container">
-        <Card title="Warnings" detail="23" />
-        <Card title="Attacks" detail="11" />
-        <Card title="Type" detail="DDoS" />
-        <div><Visualization /></div>
-        
-        
-      </div>
+      <Navbar onbuttonclick={setcurrentpage}></Navbar>
+      {currentpage == "Main" &&
+        <div className="card-container">
+          <div className="card-container">
+            <Card title="Warnings" detail="23" />
+            <Card title="Attacks" detail="11" />
+            <Card title="Type" detail="DDoS" />
+            {/* <div><Visualization /></div> */}
+          </div>
+          <div className="card-container-bottom">
+            <Card title="Visualization" detail="Maybe Bar Chart" />
+            <Card title="Visualization" detail="Maybe Line Graph" />
+
+            {/* <div><Visualization /></div> */}
+          </div>
+        </div>
+      }
+      {currentpage == "Main2" &&
+        <div>
+          Yobah's DashBoard
+          
+        </div>
+      }
+
+      {currentpage == "About" &&
+        <div className="card-container">
+          <div className="card-container">
+           About Us
+          </div>
+          
+        </div>
+      }
+
+
     </div >
   );
 };
--- FrontEnd/src/components/Navbar.js
+++ FrontEnd/src/components/Navbar.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css'; // import css file for styling
 
-const Navbar = () => {
+const Navbar = (props) => {
+  const [navlink,setnavlink] = useState("Main");
+  const handleButtonClick = (button) => {
+    setnavlink(button);
+    props.onbuttonclick(button);
+  };
   return (
     <nav className="navbar">
       <ul className="nav-links">
-        <li><a href="#">Dashboard 1</a></li>
-        <li><a href="#">Dashboard 2</a></li>
-        <li><a href="#">About Us</a></li>
+        <li><a href="#" onClick={()=>{handleButtonClick("Main")}}>Dashboard 1</a></li>
+        <li><a href="#" onClick={()=>{handleButtonClick("Main2")}}>Dashboard 2</a></li>
+        <li><a href="#" onClick={()=>{handleButtonClick("About")}}>About Us</a></li>
       </ul>
     </nav>
   );
